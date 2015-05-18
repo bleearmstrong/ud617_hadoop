5470fdaacce02cb50ead77a7034e02e2a6b1d0d5
ben <bennyska@gmail.com>
2015-05-17 23:00:20 -0700

    Correct problem statement

--- lesson_3_ex_4_red.py
+++ lesson_3_ex_4_red.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 import sys
+
 def reducer():
     hit_count = 0
- 
+    old_key = None
+
     for line in sys.stdin:
         data = line.strip().split("\t")
         if len(data) != 1:
             continue
-        if data[0] == "/assets/js/the-associates.js":
-            hit_count += 1
- 
-    print "Total hits to /assets/js/the-associates.js: ", hit_count
+        this_key = data[0]
+        if old_key and old_key != this_key:
+	    print "{0}\t{1}".format(old_key, hit_count)
+            hit_count = 0
+            old_key = this_key
+        hit_count += 1
+        old_key = this_key
+
+    print "{0}\t{1}".format(old_key, hit_count)
 
 def main():
 	import StringIO
