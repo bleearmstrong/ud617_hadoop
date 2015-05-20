9b53143e3c8ef778cf4f278f5df28b43ab86c068
ben <bennyska@gmail.com>
2015-05-19 22:34:00 -0700

    Correct code to work with combiner

--- lesson_4_weekday_sum_red.py
+++ lesson_4_weekday_sum_red.py
@@ -2,21 +2,23 @@
 import sys
 
 def reducer():
-    total = 0
-    count = 0
 
-    weekday_total = {'Sunday': 0, 'Monday': 0, 'Tuesday': 0, 'Wednesday': 0, 'Thursday': 0, 'Friday': 0, 'Saturday': 0, 'Sunday': 0}
+    old_key = None
+    total = 0
     
     for line in sys.stdin:
         
         data = line.strip().split("\t")
         if len(data) != 2:
             continue
-        day, sale = data
-        weekday_total[day] += float(sale)
-    for key in weekday_total:
-        print "Sales for ", key, ": ", weekday_total[key]
-    
+	this_key, sale = data
+	if old_key and old_key != this_key:
+	    print "{0}\t{1}".format(old_key, total)
+            total = 0
+            old_key = this_key
+        total += float(sale)
+        old_key = this_key
+    print "{0}\t{1}".format(old_key, total)
 def main():
 	import StringIO
         sys.stdin = sys.__stdin__
